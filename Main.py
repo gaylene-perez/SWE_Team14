f629517a2cfb51b3f1b9b7d3e27ea93ba62ca858
DarlaEthridge <darlae@uark.edu>
2026-02-05 14:56:02 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,6 +14,60 @@ class Main:
                 #broadcast equip. id of player hit
     #use localhost (127.0.0.1) for network addr
         #include functionality to change network addr
+    
+    import socket
+
+localIP     = "0.0.0.0"
+localPort   = 7501
+bufferSize  = 1024
+msgFromServer       = "Hello UDP Client"
+bytesToSend         = str.encode(msgFromServer)
+
+# Create a datagram socket
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Bind to address and ip
+UDPServerSocket.bind((localIP, localPort))
+
+print("UDP server up and listening")
+
+# Listen for incoming datagrams
+
+while(True):
+
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    message = bytesAddressPair[0]
+    address = bytesAddressPair[1]
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP  = "Client IP Address:{}".format(address)
+    
+    print(clientMsg)
+    print(clientIP)
+
+    # Sending a reply to client
+    UDPServerSocket.sendto(bytesToSend, address)
+
+import socket
+
+msgFromClient       = "Hello UDP Server"
+bytesToSend         = str.encode(msgFromClient)
+serverAddressPort   = ("127.0.0.1", 7501)
+bufferSize          = 1024
+
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# enable broadcasts
+socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+# Send to server using created UDP socket
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+msg = "Message from Server {}".format(msgFromServer[0])
+
+print(msg)
+
 
     #SPLASH SCREEN
     #display splash screen for 3 seconds upon startup
