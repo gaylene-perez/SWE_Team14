0ae7ae6c980ac92bb6d5fd173c718bcf2bc92601
DarlaEthridge <darlae@uark.edu>
2026-02-10 13:48:13 -0600

    Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@
 
 class Main:
 
-    #UDP SOCKETS
+    #UDP SOCKETS--> MOVED TO udp.py!!!
     #set up 2 udp sockets for transmission of data to/from players
         #socket 7500 to broadcast
             #format of transmission: single int (equip. id of player hit)
@@ -15,59 +15,7 @@ class Main:
     #use localhost (127.0.0.1) for network addr
         #include functionality to change network addr
     
-    import socket
-
-localIP     = "0.0.0.0"
-localPort   = 7501
-bufferSize  = 1024
-msgFromServer       = "Hello UDP Client"
-bytesToSend         = str.encode(msgFromServer)
-
-# Create a datagram socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-# Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
-
-print("UDP server up and listening")
-
-# Listen for incoming datagrams
-
-while(True):
-
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
-    
-    print(clientMsg)
-    print(clientIP)
-
-    # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
-
-import socket
-
-msgFromClient       = "Hello UDP Server"
-bytesToSend         = str.encode(msgFromClient)
-serverAddressPort   = ("127.0.0.1", 7501)
-bufferSize          = 1024
-
-# Create a UDP socket at client side
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-# enable broadcasts
-socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-# Send to server using created UDP socket
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-msg = "Message from Server {}".format(msgFromServer[0])
-
-print(msg)
-
+   
 
     #SPLASH SCREEN
     #display splash screen for 3 seconds upon startup
