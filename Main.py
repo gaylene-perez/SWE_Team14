a85086ad57df5da57b2b5e899d33affdabd2cf2a
katelyncraig <kacraig@uark.edu>
2026-02-10 16:03:36 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,38 +21,38 @@ class Main:
 
     #SPLASH SCREEN
     #display splash screen for 3 seconds upon startup
-from tkinter import *
+    from tkinter import *
 
-class Splash:
-    def __init__(self):
-        #setting up the blank screen
-        width = 1000
-        height = 637
+    class Splash:
+        def __init__(self):
+            #setting up the blank screen
+            width = 1000
+            height = 637
 
-        self.root = Tk()
+            self.root = Tk()
 
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
-        x = int((screen_width / 2) - (width / 2))
-        y = int((screen_height / 2) - (height / 2))
+            screen_width = self.root.winfo_screenwidth()
+            screen_height = self.root.winfo_screenheight()
+            x = int((screen_width / 2) - (width / 2))
+            y = int((screen_height / 2) - (height / 2))
 
-        #adjusting where the screen will pop up
-        self.root.geometry(f"{width}x{height}+{x}+{y}")
-        #removing the header
-        self.root.overrideredirect(True)
+            #adjusting where the screen will pop up
+            self.root.geometry(f"{width}x{height}+{x}+{y}")
+            #removing the header
+            self.root.overrideredirect(True)
 
-        # adding logo
-        self.logo = PhotoImage(file="logo.png")  
-        label = Label(self.root, image=self.logo)
-        label.pack(expand=True, fill=BOTH)
+            # adding logo
+            self.logo = PhotoImage(file="logo.png")  
+            label = Label(self.root, image=self.logo)
+            label.pack(expand=True, fill=BOTH)
 
-        #inserting the logo
-        self.root.after(3000, self.root.destroy)
-        self.root.mainloop()
+            #inserting the logo
+            self.root.after(3000, self.root.destroy)
+            self.root.mainloop()
 
 
-# Run splash screen
-Splash()
+    # Run splash screen
+    Splash()
     
 
     #PLAYER SCREEN
