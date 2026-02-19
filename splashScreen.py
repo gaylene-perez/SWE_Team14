a124ba186594711859c14d62751bc67b684af313
DarlaEthridge <darlae@uark.edu>
2026-02-19 14:18:29 -0600

    Update splashScreen.py

--- splashScreen.py
+++ splashScreen.py
@@ -1,30 +1,29 @@
 from tkinter import *
 
 class Splash:
-    def __init__(self):
-        #setting up the blank screen
+    def __init__(self, master):
+        # Use a Toplevel window over the main root
+        self.root = Toplevel(master)
+
         width = 1000
         height = 637
 
-        self.root = Tk()
-
+        # Center the splash screen
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
         x = int((screen_width / 2) - (width / 2))
         y = int((screen_height / 2) - (height / 2))
-
-        #adjusting where the screen will pop up
         self.root.geometry(f"{width}x{height}+{x}+{y}")
-        #removing the header
+
+        # Remove window decorations
         self.root.overrideredirect(True)
 
-        # adding logo
-        self.logo = PhotoImage(file="logo.png")  
+        # Load logo
+        self.logo = PhotoImage(file="logo.png")
         label = Label(self.root, image=self.logo)
         label.pack(expand=True, fill=BOTH)
 
-        #inserting the logo
+        # Destroy splash after 3 seconds
         self.root.after(3000, self.root.destroy)
-        self.root.mainloop()
 
 
