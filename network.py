fc9de96ab7fc59d6bec552566e4da3da1cf9f57f
DarlaEthridge <darlae@uark.edu>
2026-02-19 14:15:19 -0600

    Update network.py

--- network.py
+++ network.py
@@ -1,3 +1,22 @@
-switch_network(self, ip):
-  ip = input("Enter an IP address:")
-  return ip
+# This is the prototype script for the switch_network function. The official function is implemented in PlayerScreen.py
+
+import tkinter as tk #gui
+# from tkinter import *
+from tkinter import messagebox
+from dataclasses import dataclass
+from typing import List
+from tkinter import simpledialog
+
+ip = "127.0.0.1"
+
+button = tk.Button(root, text="CLICK TO SWITCH NETWORKS", command=self.switch_network, fg="blue", bg="light gray", height=2, width=25)
+        button.pack(pady=10)
+
+def switch_network(self):
+        temp = self.ip
+        self.ip = simpledialog.askstring("Input", f"Current IP address: {self.ip}\nPlease enter a valid IP address:", parent=root)
+
+        if self.ip is None:
+            self.ip = temp
+
+        return self.ip
